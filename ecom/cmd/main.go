c2900d3bd92d493c06563d43e236b8b3f1b2ceaa
connornmfinlay <cnm.finlay@gmail.com>
2025-01-26 17:04:48 +0000

    17:20

--- ecom/cmd/main.go
+++ ecom/cmd/main.go
@@ -1,12 +1,29 @@
 package main
 
 import (
-  "log"
-  "github.com/sikozonpc/ecom/cmd/api"
+	"log"
+
+	"github.com/connornmfinlay/golang/ecom/cmd/config"
+	"github.com/connornmfinlay/golang/ecom/cmd/db"
+	"github.com/go-sql-driver/mysql"
+	"github.com/sikozonpc/ecom/cmd/api"
 )
 
 func main() {
-  server := api.NewAPIServer(":8080", nil) //api.NewAPIServer 
+  db, err := db.NewMySQLStorage(mysql.Config{
+    User:     config.Envs.DBUser,
+    Passwd:   config.Envs.DBPassword,
+    Addr:     config.Envs.DBAddress,
+    DBName:   config.Envs.DBName,
+    Net:      "tcp",
+    AllowNativePasswords: true,
+    ParseTime: true,
+  })
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  server := api.NewAPIServer(":8080", db)  
   if err := server.Run(); err != nil {
     log.Fatal(err)
   }
